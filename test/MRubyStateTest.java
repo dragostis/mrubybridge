e06dfa29f1d420a028efeab764ad8aefac810ed8
Dragos Tiselice <dragostiselice@gmail.com>
2015-02-05 19:36:23 +0200

    Added simple testing foundation.

---
+++ test/MRubyStateTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MRubyStateTest {
+    @Test
+    public void testExecuteFile() throws Exception {
+
+    }
+}
---
+++ test/helpers/Identity.java
@@ -0,0 +1,171 @@
+package helpers;
+
+public class Identity {
+    public void map() {
+
+    }
+
+    public boolean map(boolean var) {
+        return var;
+    }
+
+    public byte map(byte var) {
+        return var;
+    }
+
+    public short map(short var) {
+        return var;
+    }
+
+    public char map(char var) {
+        return var;
+    }
+
+    public int map(int var) {
+        return var;
+    }
+
+    public long map(long var) {
+        return var;
+    }
+
+    public float map(float var) {
+        return var;
+    }
+
+    public double map(double var) {
+        return var;
+    }
+
+    public String map(String var) {
+        return var;
+    }
+
+    public Identity map(Identity var) {
+        return var;
+    }
+
+    public boolean[] map(boolean[] var) {
+        return var;
+    }
+
+    public byte[] map(byte[] var) {
+        return var;
+    }
+
+    public short[] map(short[] var) {
+        return var;
+    }
+
+    public char[] map(char[] var) {
+        return var;
+    }
+
+    public int[] map(int[] var) {
+        return var;
+    }
+
+    public long[] map(long[] var) {
+        return var;
+    }
+
+    public float[] map(float[] var) {
+        return var;
+    }
+
+    public double[] map(double[] var) {
+        return var;
+    }
+
+    public String[] map(String[] var) {
+        return var;
+    }
+
+    public Identity[] map(Identity[] var) {
+        return var;
+    }
+
+    public static void mapStatic() {
+
+    }
+
+    public static boolean mapStatic(boolean var) {
+        return var;
+    }
+
+    public static byte mapStatic(byte var) {
+        return var;
+    }
+
+    public static short mapStatic(short var) {
+        return var;
+    }
+
+    public static char mapStatic(char var) {
+        return var;
+    }
+
+    public static int mapStatic(int var) {
+        return var;
+    }
+
+    public static long mapStatic(long var) {
+        return var;
+    }
+
+    public static float mapStatic(float var) {
+        return var;
+    }
+
+    public static double mapStatic(double var) {
+        return var;
+    }
+
+    public static String mapStatic(String var) {
+        return var;
+    }
+
+    public static Identity mapStatic(Identity var) {
+        return var;
+    }
+
+    public static boolean[] mapStatic(boolean[] var) {
+        return var;
+    }
+
+    public static byte[] mapStatic(byte[] var) {
+        return var;
+    }
+
+    public static short[] mapStatic(short[] var) {
+        return var;
+    }
+
+    public static char[] mapStatic(char[] var) {
+        return var;
+    }
+
+    public static int[] mapStatic(int[] var) {
+        return var;
+    }
+
+    public static long[] mapStatic(long[] var) {
+        return var;
+    }
+
+    public static float[] mapStatic(float[] var) {
+        return var;
+    }
+
+    public static double[] mapStatic(double[] var) {
+        return var;
+    }
+
+    public static String[] mapStatic(String[] var) {
+        return var;
+    }
+
+    public static Identity[] mapStatic(Identity[] var) {
+        return var;
+    }
+}
