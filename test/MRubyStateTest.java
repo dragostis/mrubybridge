2f2ae85e0e4ecb93fcfc8d8c409f74868202956b
Krody Robert <krody.robi@gmail.com>
2015-02-11 21:20:45 +0200

    Added working version of maven testing

--- test/MRubyStateTest.java
+++ test/MRubyStateTest.java
@@ -1,10 +1,27 @@
-import org.junit.Test;
+import org.junit.*;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.io.IOException;
 
 public class MRubyStateTest {
-    @Test
-    public void testExecuteFile() throws Exception {
+    private MRubyState state;
 
+    @Before
+    public void setUp() {
+        state = new MRubyState();
+    }
+
+    @After
+    public void tearDown() {
+        // Finalize does not seem to work, null pointer exception
+    }
+
+
+    @Test(expected=RuntimeException.class)
+    public void testExecute_incorrectFileSyntax_errorHandling() throws RuntimeException, IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("/ruby/incorrect_synteax.rb").getFile());
+
+        state.executeFile(file);
     }
 }
