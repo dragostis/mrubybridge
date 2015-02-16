ac7592317f13eda0aad0792c40348a9838df73e3
Krody Robert <krody.robi@gmail.com>
2015-02-16 18:53:17 +0200

    Added resource loader and new exception file

--- test/MRubyStateTest.java
+++ test/MRubyStateTest.java
@@ -1,27 +1,58 @@
 import org.junit.*;
+import org.junit.rules.ExpectedException;
+import resources.helper.Loader;
 
 import java.io.File;
 import java.io.IOException;
 
 public class MRubyStateTest {
     private MRubyState state;
+    private static Loader loader;
+
+
+    @BeforeClass
+    public static void loader() {
+        loader = Loader.getInstance();
+    }
+
 
     @Before
     public void setUp() {
         state = new MRubyState();
     }
 
+
     @After
     public void tearDown() {
         // Finalize does not seem to work, null pointer exception
+        try {
+            state.finalize();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+
+
+    @Test
+    public void testExecute_errorHandling_incorrectFileSyntax() throws IOException {
+        try {
+            File file = loader.getFile("ruby/incorrect_syntax.rb");
+            state.executeFile(file);
+        } catch( RuntimeException e ) {
+            System.out.println("Testing >>");
+            System.out.print(e.getMessage());
+            System.out.println();
+        }
     }
 
 
-    @Test(expected=RuntimeException.class)
-    public void testExecute_incorrectFileSyntax_errorHandling() throws RuntimeException, IOException {
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("/ruby/incorrect_synteax.rb").getFile());
-
+    @Test(expected=IOException.class)
+    public void testExecute_errorHandling_exception() throws IOException {
+        File file = loader.getFile("ruby/exception.rb");
         state.executeFile(file);
     }
 }
---
+++ test/resources/ruby/exception.rb
@@ -0,0 +1,5 @@
+def do_s
+  2 / 0
+end
+
+p do_s
